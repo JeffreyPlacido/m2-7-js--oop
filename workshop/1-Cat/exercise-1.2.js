3908979941085e13a03e2639e02f7832a160c269
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-25 13:16:25 -0400

    E1.2

--- workshop/1-Cat/exercise-1.2.js
+++ workshop/1-Cat/exercise-1.2.js
@@ -9,3 +9,29 @@
 // and confirm that you have everything in there.
 
 // By the way, Boots is a Simaese cat...
+
+class Cat {
+    constructor(name, breed) {
+    this.species = 'cat';
+    this.breed = breed;
+    this.name = name;
+    this.tiredness = 0;
+    this.hunger = 0;
+    this.loneliness = 0;
+    this.happiness = 0;
+    }
+}
+
+const boots = new Cat('Boots', 'Siamese');
+
+console.log(boots);
+
+// Cat {
+//     species:"cat",
+//     breed:"Siamese",
+//     name:"Boots",
+//     tiredness:0,
+//     hunger:0,
+//     loneliness:0,
+//     happiness:0
+//     }
